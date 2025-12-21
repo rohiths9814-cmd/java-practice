e09e5ed586ae0e59f87bb52957ac555d6eeed25a
rohiths9814-cmd <rohiths9814@gmail.com>
2025-12-21 15:30:15 +0530

    first exercise

---
+++ day2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class day2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.print("Enter your grade: ");
+        char grade = sc.next().charAt(0);
+
+        System.out.print("Enter your percentage: ");
+        float percentage = sc.nextFloat();
+
+        System.out.print("Is passed ");
+        boolean ispassed = sc.nextBoolean();
+
+
+        System.out.println("Hello " + name + ", your are " +  age + " years old.");
+        System.out.println("Grade : " +grade );
+        System.out.println("Percentage : " +percentage );
+        System.out.println("Is passed : " +ispassed );
+    }
+
+}
