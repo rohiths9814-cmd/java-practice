98e3677a0326d37d556d4a48385b02115c564262
rohiths9814-cmd <rohiths9814@gmail.com>
2025-12-21 15:39:43 +0530

    New exercise

---
+++ day7t6.java
@@ -0,0 +1,22 @@
+public class day7t6 {
+    public static boolean isprime(int n){
+        if(n <= 1){
+            return false;
+        }
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        int number = 29;
+        if(isprime(number)){
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
