c3444c972b8bec4d8e8fd8dad049e11a5effa794
rohiths9814-cmd <rohiths9814@gmail.com>
2025-12-21 15:41:06 +0530

    New exercise

---
+++ MyCode.java
@@ -0,0 +1,5 @@
+public class MyCode{
+    public static void main(String[] args){
+        System.out.println("I noticed that if I chaneged the class name it autometically change the file name also.");
+    }
+}
