a0d8df3ed1e0834d4828f6dd17e99a4906afced7
rohiths9814-cmd <rohiths9814@gmail.com>
2025-12-22 17:22:13 +0530

    New contibution

--- MyCode.java
+++ MyCode.java
@@ -1,5 +1,6 @@
 public class MyCode{
     public static void main(String[] args){
-        System.out.println("I noticed that if I chaneged the class name it autometically change the file name also.");
+        System.out.println("I noticed that if I changed the class name it automatically changes the file name also.");
+        System.out.println("Git is very useful to track changes.");
     }
 }
