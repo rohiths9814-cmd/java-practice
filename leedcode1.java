52e3e7d47dc12cfa3159a8acd09158c6e6284bfa
rohiths9814-cmd <rohiths9814@gmail.com>
2025-12-23 08:40:56 +0530

    This is the updated version of code with function

--- leedcode1.java
+++ leedcode1.java
@@ -3,20 +3,25 @@ import java.util.Scanner;
 public class leedcode1 {
     public static void main(String[] args) {
         
-        int n,product=1,sum=0;
+        int a;
         Scanner in = new Scanner(System.in);
 
-       n= in.nextInt();
+        a= in.nextInt();
 
-       while(n>0){
+       System.out.print(subractProductAndSum(a));
+    }
+
+    static int subractProductAndSum(int n){
+
+        int product=1,sum=0;
+
+        while(n>0){
         int digit=n%10;
         product*=digit;
         sum+=digit;
         n/=10;
        }
 
-       int result = product - sum;
-
-       System.out.print(result);
+       return product-sum;
     }
 }
