70fe2924262533c9c08336d242203c4e76cbc3e6
rohiths9814-cmd <rohiths9814@gmail.com>
2025-12-27 16:59:27 +0530

    Using append in string practice

---
+++ stringBuilder.java
@@ -0,0 +1,15 @@
+public class stringBuilder {
+    public static void main(String[] args) {
+        
+        StringBuilder demo = new StringBuilder();
+
+        demo.append("J");
+        demo.append("a");
+        demo.append("v");
+        demo.append("a");
+
+        System.out.print(demo);
+
+
+    }
+}
