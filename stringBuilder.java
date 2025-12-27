cd25665afc33896368874ac7924b78256396954a
rohiths9814-cmd <rohiths9814@gmail.com>
2025-12-27 17:11:49 +0530

    Manual reverse with for loop for reverse the string and append in StringBuilder

--- stringBuilder.java
+++ stringBuilder.java
@@ -1,9 +1,14 @@
 public class stringBuilder {
     public static void main(String[] args) {
-        
-        StringBuilder demo = new StringBuilder("JavaXYZWorld");
 
-        demo.delete(4,7);
+        String example = "Welcome";
+        
+        StringBuilder demo = new StringBuilder();
+
+        for (int i = example.length() - 1; i >=0; i--) {
+            demo.append(example.charAt(i));
+        }
+
         System.out.print(demo);
     }
 }
