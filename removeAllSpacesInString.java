c247d56caffae3eb0f0662577ff2cb918199f503
rohiths9814-cmd <rohiths9814@gmail.com>
2025-12-29 17:57:31 +0530

    removeAllSpacesInString is solved

---
+++ removeAllSpacesInString.java
@@ -0,0 +1,10 @@
+public class removeAllSpacesInString {
+    public static void main(String[] args) {
+        
+        String a = "Java is very easy";
+
+        a = a.replace(" ","");
+
+        System.out.println(a);
+    }
+}
