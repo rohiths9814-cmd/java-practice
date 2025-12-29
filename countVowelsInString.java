4c4f108f783736ecb96ace81224b55ee4d8409a0
rohiths9814-cmd <rohiths9814@gmail.com>
2025-12-29 17:48:00 +0530

    Improved

--- countVowelsInString.java
+++ countVowelsInString.java
@@ -8,7 +8,9 @@ public class countVowelsInString {
         a = a.toLowerCase();
 
         for (int i = 0; i < a.length(); i++) {
-            if(a.charAt(i) == 'a' || a.charAt(i) == 'e' || a.charAt(i) == 'i' || a.charAt(i) == 'o' || a.charAt(i) == 'u'){
+
+            char ch = a.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
                 count++;
             }
         }
