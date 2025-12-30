d8de777afaa1582cab1c3d40751ecb97e7467de5
rohiths9814-cmd <rohiths9814@gmail.com>
2025-12-30 19:34:42 +0530

    Arrays practice

---
+++ Array/printArray.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class printArray {
+    public static void main(String[] args) {
+        
+        int[] arr = {10, 20, 30,40};
+
+        for(int i=0; i<arr.length ; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ Array/toString.java
@@ -0,0 +1,12 @@
+
+import java.util.Arrays;
+
+public class toString {
+    public static void main(String[] args) {
+
+        int[] a = {3, 1, 2};
+        Arrays.sort(a);
+        System.out.println(Arrays.toString(a));
+        
+    }
+}
