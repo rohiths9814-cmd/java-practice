af460ddee7990e1c060ec3138f552947441e5b17
rohiths9814-cmd <rohiths9814@gmail.com>
2025-12-31 18:44:49 +0530

    New form

--- Array/linearSearch.java
+++ Array/linearSearch.java
@@ -3,18 +3,16 @@ public class linearSearch {
         
         int[] arr = {5, 10, 15};
 
-        int index = -1;
-
         int target = 5;
 
         for(int i = 0 ; i < arr.length ; i++){
 
             if(arr[i] == target){
-                index = i;
+                System.out.println("Found at index "+ i);
                 break;
+            }else{
+                System.out.println("Not found");
             }
         }
-
-        System.out.println(index);
     }
 }
