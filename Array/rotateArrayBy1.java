017c283c105a89316ca50c650e25e84c9ecf0b71
rohiths9814-cmd <rohiths9814@gmail.com>
2026-01-01 14:15:19 +0530

    rotateArrayBy1 is practiced

---
+++ Array/rotateArrayBy1.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class rotateArrayBy1 {
+    public static void main(String[] args) {
+        
+        int[] arr = {1,2,3,4,5};
+
+        int temp = arr[0];
+
+        for(int i = 0 ; i < arr.length - 1 ; i++){
+            arr[i] = arr[i+1];
+        }
+
+        arr[arr.length - 1] = temp;
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
