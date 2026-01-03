ae4477439b7e6a6090feed3fc5d771e8ed25e025
rohiths9814-cmd <rohiths9814@gmail.com>
2026-01-03 19:06:55 +0530

    .

---
+++ NewYear2026.java
@@ -0,0 +1,7 @@
+public class NewYear2026 {
+    public static void main(String[] args) {
+        
+        System.out.println("Happy start of 2026. From the day 1 we learn all the skills like DSA,Web development,Security. Be ready to crack the top MNC company like Google, Microsoft, Amazon. Work Hard in 2026 and be a great person.");
+
+    }
+}
