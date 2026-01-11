5273ba4d43a055e742b40e0b06b19aaa1bf6a579
rohiths9814-cmd <rohiths9814@gmail.com>
2026-01-11 19:23:01 +0530

    Using of method in OOP

---
+++ OOP/Greeter.java
@@ -0,0 +1,12 @@
+public class Greeter {
+
+    public void sayHello(){
+        System.out.println("Hello java");
+    }
+    public static void main(String[] args) {
+        
+        Greeter g1 = new Greeter();
+
+        g1.sayHello();
+    }
+}
